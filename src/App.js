2998b0cae75f5bfb785613824c5d27c995878dae
Sai Nithish <nithishsai10@gmail.com>
2021-08-29 15:01:42 +0530

    final working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 import BlogService from './services/BlogService';
 import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalList';
 import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
@@ -6,10 +6,66 @@ import './App.css';
 import ListBlogComponent from './components/ListBlogComponent';
 import { BrowserRouter as Router , Route, Switch } from 'react-router-dom';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { API , Storage  } from 'aws-amplify';  
+import { listTodos } from './graphql/queries';
+import { createTodo as createNoteMutation, deleteTodo as deleteNoteMutation } from './graphql/mutations';
+import "./components/UI/Button/Button.css";
+const initialFormState = { name: '', description: '' }
 const App = () => {
   const [courseGoals, setCourseGoals] = useState([
     
 ]);
+
+// ********************************************************
+const [notes, setNotes] = useState([]);
+const [formData, setFormData] = useState(initialFormState);
+
+useEffect(() => {
+  fetchNotes();
+}, []);
+
+useEffect(() => {
+  fetchNotes();
+}, []);
+
+async function fetchNotes() {
+  const apiData = await API.graphql({ query: listTodos });
+  const notesFromAPI = apiData.data.listTodos.items;
+  await Promise.all(notesFromAPI.map(async note => {
+    if (note.image) {
+      const image = await Storage.get(note.image);
+      note.image = image;
+    }
+    return note;
+  }))
+  setNotes(apiData.data.listTodos.items);
+}
+
+async function createNote() {
+  if (!formData.name || !formData.description) return;
+  await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+  if (formData.image) {
+    const image = await Storage.get(formData.image);
+    formData.image = image;
+  }
+  setNotes([ ...notes, formData ]);
+  setFormData(initialFormState);
+}
+
+async function deleteNote({ id }) {
+  const newNotesArray = notes.filter(note => note.id !== id);
+  setNotes(newNotesArray);
+  await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
+}
+
+async function onChange(e) {
+  if (!e.target.files[0]) return
+  const file = e.target.files[0];
+  setFormData({ ...formData, image: file.name });
+  await Storage.put(file.name, file);
+  fetchNotes();
+}
+//***************************************************************
 // { title: 'Blog 1', content:'This is from React ' , id:'1'},
 
   const addGoalHandler = (enteredTitle,enteredContent) => {
@@ -46,7 +102,7 @@ const App = () => {
   }
   return (
     <div>
-      <Router>
+      {/* <Router>
                
           <section id="goal-form">
             <CourseInput onAddGoal={addGoalHandler} />
@@ -62,8 +118,54 @@ const App = () => {
             
           </section>
        
-      </Router> 
+      </Router>  */}
+
+  <section id="goal-form">
+      <div  className="form-control">
+      <h1>Add New Blog</h1>
+      <div className="form-control">
+      <label>Blog Title:</label>
+      <input
+        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+        placeholder="Name Your Blog"
+        value={formData.name}
+      />
+      <br/>
+      <label >Content:</label>
+      <input
+        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+        placeholder="Enter Blog Content"
+        value={formData.description}
+      />
+      <br/>
+      <input
+         type="file"
+        onChange={onChange}
+      />
+      </div>
+      <button  className='button' onClick={createNote}>Publish Blog</button>
+      <br/>
       <AmplifySignOut />
+      
+      
+      
+    </div>
+      </section>
+      <div  className="goal-form" style={{marginBottom: 30}}>
+        {
+          notes.map(note => (
+            <div key={note.id || note.name}>
+              <h2>{note.name}</h2>
+              <p>{note.description}</p>
+              <button className="button" onClick={() => deleteNote(note)}>Delete Blog</button>
+              {
+        note.image && <img src={note.image} style={{width: 400}} />
+      }
+            </div>
+          ))
+        }
+      </div>
+      
     </div>
   );
 };
